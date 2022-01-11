fc03a682874be4ad114b43ddd4f2db00e25d5e7c
alira <alira@CYBERSPACE>
2022-01-11 23:38:38 +0500

    Add project files.

---
+++ Ali-Raza083/DataDb/StoreDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ali_Raza083.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ali_Raza083.DataDb
+{
+    public class StoreDb : DbContext
+    {
+        public StoreDb(DbContextOptions<StoreDb> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
